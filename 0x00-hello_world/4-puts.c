957cc30c680ab6b96021116f50548d101c3942b3
simonteclai <nomistec.st@gmail.com>
2022-09-08 15:35:12 -0700

    5

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,6 @@
 include <stdio.h>
 int main(void)
 {
-	char the[] = "Programing is like building a multilingual puzzle";
-	puts(the);
+	puts("\"Programing is like building a multilingual puzzle");
 	return(0);
 }
