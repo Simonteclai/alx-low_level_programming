28a0c959a291b495495dd1acdac1b02e47a5a7a0
simonteclai <nomistec.st@gmail.com>
2022-09-08 18:23:54 -0700

    0

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,12 +3,11 @@
 /**
  * 
  * main - Entry piont
- *Discription: prints "\" prongramming is like building a multilingual puzzel|*
+ * Discription: prints "\" prongramming is like building a multilingual puzzel|*
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-
 puts("\"Programming is like building a multilingual puzzle");
 return(0);
 }
