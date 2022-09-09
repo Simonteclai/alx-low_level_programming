95f0e9cd3054000c4ceeef2c5cc1791b29744135
simonteclai <nomistec.st@gmail.com>
2022-09-09 13:50:40 -0700

    2

---
+++ 0x01-variables_if_else_while/4-alphabt.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	char a;
+
+	for (a = 'a'; a <= 'z'; a != e && a != q; a++)
+	putchar(a);
+	putchar('\n');
+	return (0);
+}
