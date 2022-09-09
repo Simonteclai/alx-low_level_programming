861a4eac5730385d2d92d6188f9500c7451537dc
simonteclai <nomistec.st@gmail.com>
2022-09-09 16:06:57 -0700

    4

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	int s;
+
+	for (s = 0; s < 10; s++)
+	{
+		putchar(s + '0');
+		if (s < 10)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
