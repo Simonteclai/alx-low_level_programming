f857dd0985bc2348d19f5f515b11242c84b14467
simonteclai <nomistec.st@gmail.com>
2022-09-13 16:46:08 -0700

    10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <math.h>
+/**
+ * add - adds the given numbers
+ * @a: first oprand
+ * @b: second oprand to be added
+ * Return: the result
+ */
+int add(int a, int b)
+{
+	int result = a + b;
+
+	_putchar(result + '0');
+	return (result);
+}
