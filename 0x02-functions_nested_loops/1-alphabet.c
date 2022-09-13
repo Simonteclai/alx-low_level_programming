38d59c886615ce01d5aacfd492a089d4d3385116
simonteclai <nomistec.st@gmail.com>
2022-09-13 09:57:33 -0700

    1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ * Description: prints lowercase alphabets
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+		_putchar(c);
+		_putchar('\n');
+	return (0);
+}
