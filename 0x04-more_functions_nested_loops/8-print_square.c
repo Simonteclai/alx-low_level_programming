3f384e77e056d31d1dee5df01672bd08efa89fdb
simonteclai <nomistec.st@gmail.com>
2022-09-15 16:17:36 -0700

    tt

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_square - Prints the character in square form
+ * @n: number of times the character is printed
+ * Return: void
+ */
+
+void print_square(int n)
+{
+	int i = 0, j;
+
+	while (i < n && n > 0)
+	{
+		j = 0;
+		while (j < n)
+		{
+			_putchar('#');
+			j++;
+		}
+		_putchar('\n');
+		i++;
+	}
+	if (i == 0)
+		_putchar('\n');
+}
