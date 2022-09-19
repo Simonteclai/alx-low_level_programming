a38b808897fc1f8b3fab680e5bf88c95f76f68ff
simonteclai <nomistec.st@gmail.com>
2022-09-19 16:04:49 -0700

    eway

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time>
+
+/**
+ * main - gets key
+ * Return: 0 always
+ */
+int main(void)
+{
+	int b = 0, d = 0;
+	time_t t;
+
+	srand((unsigned int) time(&t);
+			while (d < 2772)
+			{
+			b = rand() % 128;
+			if ((d + b) > 2772)
+			break;
+			d = d + b;
+			printf("%c", b);
+			}
+			printf("%c\n", (2772 - c));
+			return (0);
+}
