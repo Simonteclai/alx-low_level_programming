91b7c921370067d8ee1ceb8602e2c0fb87e959d1
simonteclai <nomistec.st@gmail.com>
2022-09-19 14:43:48 -0700

    8

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_array - prints the given number in an array
+ * @a: array ints
+ * @n: the number of elements
+ */
+
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", a[i]);
+		if (i == n - 1)
+			continue;
+		printf(", ");
+	}
+	printf("\n");
+}
