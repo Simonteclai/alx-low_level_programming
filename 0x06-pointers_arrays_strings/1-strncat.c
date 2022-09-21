66a755624f54a596ac6eb856db1dcbbc05eafca0
simonteclai <nomistec.st@gmail.com>
2022-09-21 09:31:44 -0700

    1

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -10,7 +10,16 @@
 
 char *_strncat(char *dest, char *src, int n)
 {
-	_strncat = strncat(dest, src, n);
-	return (_strncat);
+	int i, j;
+
+	for (i = 0; dest[i] != '\0'; i++)
+		;
+	for (j = 0; src[j] != '\0'; j++)
+	{
+		while (j <= n)
+			dest[i] = src[j];
+		i++;
+	}
+	return (dest);
 }
 
