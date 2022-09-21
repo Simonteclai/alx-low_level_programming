b2c236d14a2bf654cbc7e76b4f3a26f6683ae78d
simonteclai <nomistec.st@gmail.com>
2022-09-21 07:51:16 -0700

    0

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _strcat - comcaterates two strings
+ * @dest: string one
+ * @src: string two
+ * Return: *_strcat
+ */
+
+char *_strcat(char *dest, char *src);
+{
+	*_strcat = strcat(*dest, *src);
+	return (*_strcat);
+}
