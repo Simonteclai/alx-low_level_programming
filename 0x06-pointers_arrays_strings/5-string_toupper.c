befcd6693784d5379a83cf21ee2e70c388f71ef3
simonteclai <nomistec.st@gmail.com>
2022-09-21 15:25:50 -0700

    5

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -2,8 +2,18 @@
 
 /**
  * string_toupper - changes the string to upper
+ * @str: the string
+ * Return: str
  */
 
-char *string_toupper(char *)
+char *string_toupper(char *str)
 {
-	
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (str[i] >= 97 && str[i] <= 122)
+			str[i] = str[i] - 32;
+	}
+	return (str);
+}
