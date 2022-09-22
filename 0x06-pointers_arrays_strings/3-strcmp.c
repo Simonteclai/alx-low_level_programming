9a6dff84c32d84cfd52891ac2c61beb2a5d068ec
simonteclai <nomistec.st@gmail.com>
2022-09-22 08:14:26 -0700

    3

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -12,7 +12,7 @@ int _strcmp(char *s1, char *s2)
 	char *st1 = s1;
 	char *st2 = s2;
 
-	while (*s1 != '\0' && *s2 != '\0' && *s1 == *s2)
+	while (*st1 != '\0' && *st2 != '\0' && *st1 == *st2)
 	{
 		st1++;
 		st2++;
