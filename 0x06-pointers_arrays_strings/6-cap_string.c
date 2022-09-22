5ae3eefe822a394ad50134a628f579627bc01c02
simonteclai <nomistec.st@gmail.com>
2022-09-22 10:34:43 -0700

    6

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -8,12 +8,24 @@
 
 char *cap_string(char *x)
 {
-	int i;
+	int count = 0, i;
+	int sep_words[] = {32, 9, 10, 44, 59, 46, 33, 63, 34, 40, 123, 125};
 
-	for (i = 0; x[i] != '\0'; i++)
+	if (*(x + count) >= 97 && *(x + count) <= 122)
+		*(x + count) = *(x + count) - 32;
+	count++;
+	while (*(x + count) != '\0')
 	{
-		if (x[i] >= 97 && x[i] < 122)
-			x[i] = x[i] - 32;
+		for (i = 0; i < 13; i++)
+		{
+			if (*(x + count) == sep_words[i])
+			{
+				if ((*(x + (count + 1)) >= 97) && (*(x + (count + 1)) <= 122))
+					*(x + (count + 1)) = *(x + (count + 1)) - 32;
+				break;
+			}
+		}
+		count++;
 	}
 	return (x);
 }
