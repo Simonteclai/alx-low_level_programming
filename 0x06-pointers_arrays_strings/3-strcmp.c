241a334723d280e7bfa6ce4110a9b94057e619d4
simonteclai <nomistec.st@gmail.com>
2022-09-22 02:03:45 -0700

    3

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -9,10 +9,13 @@
 
 int _strcmp(char *s1, char *s2)
 {
-	for (; (*s1 != '\0' && *s2 != '\0') && (*s1 == *s2); s1++; s2++)
-		;
-	if (*s1 == *s2)
+	char *st1 = s1;
+	char *st2 = s2;
+
+	while (*s1 != '\0' && *s2 !='\0' && *s1 == *s2)
 	{
-		return (0);
+		st1++;
+		st2++;
 	}
+	return (*st1 - *st2)
 }
