ed7efad5c1e6a9c31c52e3d073afda9cf8ebfade
simonteclai <nomistec.st@gmail.com>
2022-09-26 08:35:37 -0700

    1

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -11,4 +11,15 @@
 
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
+	int i;
+
+	for (i = 0; i < n && src[i] != '\0'; i++)
+		dest[i] = src[i];
+	for ( ; i < n; I++)
+		dest[i] = '\0';
+
+	return (dest);
+}
+
+
 
