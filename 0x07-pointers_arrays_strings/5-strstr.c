17970d74e7a64e404cd921d8870be87da362da96
simonteclai <nomistec.st@gmail.com>
2022-09-26 14:29:34 -0700

    5

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * _strstr - finds substring
+ * @haystack: string
+ * @needle:
+ * Return: val
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	char *st1, *st2;
+
+	while (*haystack != '\0')
+	{
+		st1 = haystack;
+		st2 = needle;
+
+		while (*haystack != '\0' && *st2 != '\0' && *haystack == *st2)
+		{
+			haystack++;
+			st2++;
+		}
+		if (*st2 == '\0')
+			return (st1);
+		haystack = st1 + 1;
+	}
+	return (0);
+}
