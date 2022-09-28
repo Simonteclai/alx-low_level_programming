65a570216106d298d2c749b927b592e328c45cd1
simonteclai <nomistec.st@gmail.com>
2022-09-28 13:54:10 -0700

    6

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,6 +1,28 @@
 #include "main.h"
 
 /**
+ * _test - tester
+ * @n: the div
+ * @pr: num
+ * Return: val
+ */
+int _test(int n, int pr)
+{
+	if (pr == n - 1)
+	{
+		return (1);
+	}
+	else if (n % pr == 0)
+	{
+		return (0);
+	}
+	else if (n % pr != 0)
+	{
+		return (_test(n, pr + 1));
+		return (0);
+	}
+
+/**
  * is_prime_number - checks if the number is prime number
  * @n: number to be checked
  * Return: 0 or 1
@@ -8,14 +30,11 @@
 
 int is_prime_number(int n)
 {
-	if (n == 1 || n < 0)
-	{
+	int pr = 2;
+
+	if (n < 2)
 		return (0);
-	}
-	else if (n % 2 == 0 || n % 3 == 0 || n % 7 == 0 || n % 9 == 0 || n % 5 == 0)
-	{
-		return (0);
-	}
-	else
+	if (n == 2)
 		return (1);
+	return (_test(n, pr));
 }
