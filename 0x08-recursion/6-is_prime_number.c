e1b283242a9ea03e284952aa255f8d8a65297812
simonteclai <nomistec.st@gmail.com>
2022-09-28 12:31:37 -0700

    6

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -7,8 +7,14 @@
 
 int is_prime_number(int n)
 {
-	if ((n % 2) > 0)
-		return (1);
-	else
+	if (n == 1 || n < 0)
+	{
 		return (0);
+	}
+	else if (n % 2 == 0 || n % 3 == 0 || n % 7 == 0 || n % 9 == 0 || n % 5 == 0)
+	{
+		return (0);
+	}
+	else
+		return (1);
 }
