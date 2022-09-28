7ea459e2ea5cb36a8deecc21e211a6a534c5d869
simonteclai <nomistec.st@gmail.com>
2022-09-28 15:52:09 -0700

    10

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -5,32 +5,34 @@
  * @s: pointer
  * Return: val
  */
- int _strlen_recursion(char *s)
+int _strlen_recursion(char *s)
 {
-	if (!*s)
+	if (*s != '\0')
 	{
-		return (0);
-	}
 	return (1 + _strlen_recursion(s + 1));
+	}
+	return (0);
 }
 
 /**
  * p - palidrome
  * @s: character
  * @l: int
+ * @i: int
  * Return: value
  */
-int p(char *s, int l)
+int p(char *s, int l, int i)
 {
-	if (l < 1)
+	if (i < 1 && s[i] == s[l])
 	{
-		return (1);
+		return (p(s, l - 1, i + 1));
 	}
-	if (*s == *s(s + 1))
+	else if (s[i] != s[l])
 	{
-		return (p(s + 1, l - 2));
+		return (0);
 	}
-	return (0);
+	else
+	return (1);
 }
 
 /**
@@ -41,7 +43,8 @@ int p(char *s, int l)
 
 int is_palindrome(char *s)
 {
-	int len = _strlen_recursion(s);
-	
-	return (p(s, len - 1));
+	int i = 0;
+	int len = _strlen_recursion(s) - 1;
+
+	return (p(s, len, i));
 }
