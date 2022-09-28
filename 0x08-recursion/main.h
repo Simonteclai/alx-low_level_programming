8a34f4fc530a7f03ee3aa9cdf4609ff2df2f6582
simonteclai <nomistec.st@gmail.com>
2022-09-28 15:24:54 -0700

    11

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -6,3 +6,5 @@ int _pow_recursion(int x, int y);
 int _strlen_recursion(char *s);
 void _print_rev_recursion(char *s);
 int _sqrt_recursion(int n);
+int wildcmp(char *s1, char *s2);
+int is_palindrome(char *s);
