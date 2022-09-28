8abbf80255c51bc37974687909a8a558aa692385
simonteclai <nomistec.st@gmail.com>
2022-09-28 13:22:53 -0700

    c

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,6 +1,25 @@
 #include "main.h"
 
 /**
+ * _sqr - find sqr
+ * @i: num
+ * @n: num
+ * Return: val
+ */
+int _sqr(int i, int n)
+{
+	if (n < 0)
+		return (-1);
+	else if (i * i == n)
+		return (i);/
+	else if (i * i < n)
+	{
+		return (_sqr(i + 1, n));
+	}
+	return (-1);
+}
+
+/**
  * _sqrt_recursion - finds the squar root
  * @n: the number
  * Return: val or -1
@@ -8,10 +27,12 @@
 
 int _sqrt_recursion(int n)
 {
-	if (n > 0)
+	int i = 0;
+
+	if (i < 0)
 	{
-		return (n ^ (1 / 2));
+		return (-1);
 	}
 	else
-		return (-1);
+		return (_sqr(i, n));
 }
