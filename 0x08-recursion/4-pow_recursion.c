a30acbccbd17ac8abba6cac0322d2169ee246097
simonteclai <nomistec.st@gmail.com>
2022-09-28 06:10:58 -0700

    4

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - shows x the power of y
+ * @x: number
+ * @y: power
+ * Return: value or -1
+ */
+
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+		return (-1);
+	else
+		return (x ^ y);
+}
