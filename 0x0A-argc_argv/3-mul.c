b6d200e14e0adc6db0e34c378a7d4ab62a41310d
simonteclai <nomistec.st@gmail.com>
2022-09-30 16:48:30 -0700

    3

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,3 +1,4 @@
+#include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -10,20 +11,16 @@
 
 int main(int argc, char *argv[])
 {
-	int i, j, mul;
+	int i, j;
 
-	mul = 0;
-
-	if (argc != 3)
+	if (argc == 3)
 	{
-		printf("error\n");
-		return (1);
+		i = atoi(argv[1]);
+		j = atoi(argv[2]);
+		
+		printf("%d\n", i * j);
+		return (0);
 	}
-
-	i = atoi(argv[1]);
-	j = atoi(argv[2]);
-	mul = i * j;
-
-	printf("%d\n", mul);
+	printf("Error\n");
 	return (0);
 }
