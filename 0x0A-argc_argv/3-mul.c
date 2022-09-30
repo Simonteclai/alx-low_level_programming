9a9a035ba1cfe5ac619ff831c1788ce3af7a17df
simonteclai <nomistec.st@gmail.com>
2022-09-30 11:05:00 -0700

    2

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main -  multiplies two arguments
+ * @argv: vector
+ * @argc: arguments
+ * Return: 0 or 1
+ */
+
+int main(int argc, char *argv[])
+{
+	int mul;
+
+	if (argc == 2)
+	{
+		mul = *argv[1] * *argv[2];
+		printf("%d\n", mul);
+		return (0);
+	}
+	else
+	{
+		printf("error\n");
+		return (1);
+	}
+}
