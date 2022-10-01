75bfb56b2e4620414c9ef3267bb9869680cc35cf
simonteclai <nomistec.st@gmail.com>
2022-09-30 17:17:49 -0700

    100

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -30,7 +30,7 @@ int main(int argc, char *argv[])
 	}
 	while (coins[position] != '\0')
 	{
-		if (total >= coins[positions])
+		if (total >= coins[position])
 		{
 			aux = (total / coins[position]);
 			change += aux;
