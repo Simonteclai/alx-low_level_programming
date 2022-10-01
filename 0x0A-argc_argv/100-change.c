178570eb51b765cd7e172a2ba0b06e7361674359
simonteclai <nomistec.st@gmail.com>
2022-09-30 18:27:13 -0700

    100

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -3,41 +3,48 @@
 #include <stdlib.h>
 
 /**
- * main - changes
+ * main - prints changes
  * @argc: arguments
- * @argv: vectors
+ * @argv: vector
  * Return: val
  */
 
-int main(int argc, *argv[])
+int main(int argc, char *argv[])
 {
 	int cents, coins = 0;
 
-	if (argc == 1 || argc > 2)
+	if (argc != 2)
 	{
 		printf("Error\n");
 		return (1);
 	}
 
 	cents = atoi(argv[1]);
+
 	while (cents > 0)
 	{
-		if (cents >= 25)
+		coins++;
+		if ((cents - 25) >= 0)
+		{
 			cents -= 25;
-
-		else if (cents >= 10)
+			continue;
+		}
+		if ((cents - 10) >= 0)
+		{
 			cents -= 10;
-
-		else if (cents >= 5)
+			continue;
+		}
+		if ((cents - 5) >= 0)
+		{
 			cents -= 5;
-
-		else if (cents >= 2)
+			continue;
+		}
+		if ((cents - 2) >= 0)
+		{
 			cents -= 2;
-
-		else if (cents >= 1)
-			cents -= 1;
-
-		coins += 1;
+			continue;
+		}
+		cents--;
 	}
 	printf("%d\n", coins);
 	return (0);
