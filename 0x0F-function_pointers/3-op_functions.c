33529a0b3fc8f81c5edaa491cb9958aa23e45a81
simonteclai <nomistec.st@gmail.com>
2022-10-12 14:01:37 -0700

    3

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,61 @@
+#include "3-calc.h"
+
+/**
+ * op_add - the add
+ * @a: int one
+ * @b: int two
+ * Return: sum
+ */
+
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - subtructer
+ * @a: int one
+ * @b: int two
+ * Return: sub
+ */
+
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - multiplier
+ * @a: int
+ * @b: int
+ * Return: mul
+ */
+
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - divider
+ * @a: int
+ * @b: int
+ * Return: div
+ */
+
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - modular
+ * @a: int
+ * @b: int
+ * Return: mod
+ */
+
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
