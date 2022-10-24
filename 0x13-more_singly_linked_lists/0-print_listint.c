539fe67d95274d82e582c49719a56827f713af80
simonteclai <nomistec.st@gmail.com>
2022-10-24 14:33:20 -0700

    0

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * print_listint - counts number of nodes
+ * @h: pointer
+ * Return: val
+ */
+
+size_t print_listint(const listint_t *h)
+{
+	size_t count = 0;
+
+	while (h)
+	{
+		printf("%d\n", h->n);
+		count++;
+		h = h->next;
+	}
+	return (count);
+}
