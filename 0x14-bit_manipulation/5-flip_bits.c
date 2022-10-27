29baaed2e4ebbedca0e38babbce9ff59da1fcce7
simonteclai <nomistec.st@gmail.com>
2022-10-27 13:05:38 -0700

    5

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * flip_bits - flips bits
+ * @n: value one
+ * @m: value two
+ * Return: val
+ */
+
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned int nb;
+
+	for (nb = 0; n || m; n >> 1, m >> 1)
+	{
+		if ((n & 1) != (m & 1))
+			nb++;
+	}
+	return (nb);
+}
