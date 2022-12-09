9ce2e76639af69142ce2fb1beb135631ea3693f3
simonteclai <nomistec.st@gmail.com>
2022-12-09 11:23:51 -0800

    1

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,29 @@
+nclude "lists.h"
+
+/**
+* dlistint_len - returns num of elements
+* @h: head of list
+* Return: num of elem
+*/
+
+size_t dlistint_len(const dlistint_t *h)
+
+{
+
+	size_t num = 0;
+
+
+
+	while (h != NULL)
+
+	{
+
+		h = h->next;
+
+		num++;
+
+	}
+
+	return (num);
+
+}
